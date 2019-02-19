60a87a3c9ce79562197cedc88179f20bd6b1e8e7
MaksimovAndrey <andreymak123@mail.ru>
2019-02-19 10:34:09 +0300

    Класс Triangle v2

--- Maksimov.AA/Task9/Source8.cpp
+++ Maksimov.AA/Task9/Source8.cpp
@@ -13,10 +13,6 @@ public:
 	double perimeter, area;
 	int existence, type;
 	Triangle(double _a, double _b, double _c); // c - диагональ
-	int Existence(); // существование
-	double Perimeter(); // периметр
-	double Area(); // площадь
-	int Type(); // тип
 	Triangle();
 	~Triangle();
 };
@@ -26,48 +22,33 @@ Triangle::Triangle()
 	b = 0.0;
 	c = 0.0;
 }
-int Triangle::Type()
-{
-	if (c*c == a * a + b * b)
-		type = 0; // прямоугольный
-	if (c*c > a * a + b * b)
-		type = 1; // тупоугольный
-	if (c*c < a * a + b * b)
-		type = 1; // остроугольный
-	return type;
-}
-double Triangle::Perimeter()
-{
-	perimeter = a + b + c;
-	return perimeter;
-}
-double Triangle::Area()
-{
-	area = sqrt((0.5 * (a + b + c))*(0.5 * (a + b + c) - a)*(0.5 * (a + b + c) - b)*(0.5 * (a + b + c) - c));
-	return area;
-}
 Triangle::Triangle(double _a, double _b, double _c)
 {
 	a = _a;
 	b = _b;
 	c = _c;
-}
-int Triangle::Existence()
-{
 	if (a == 0.0 || b == 0.0 || c == 0.0)
-	{
 		existence = -1; // треугольник не задан
-		return -1;
-	}
 	if (a + b > c && b + c > a && a + c > b)
-	{
 		existence = 1; // треугольник существует
-		return 1;
+	else
+		existence = 0; // треугольник не существует
+	if (existence == 1)
+	{
+		if (c*c == a * a + b * b)
+			type = 0; // прямоугольный
+		if (c*c > a * a + b * b)
+			type = 1; // тупоугольный
+		if (c*c < a * a + b * b)
+			type = 2; // остроугольный
+		area = sqrt((0.5 * (a + b + c))*(0.5 * (a + b + c) - a)*(0.5 * (a + b + c) - b)*(0.5 * (a + b + c) - c));
+		perimeter = a + b + c;
 	}
 	else
 	{
-		existence = 0; // треугольник не существует
-		return 0;
+		type = -1;
+		area = 0;
+		perimeter = 0;
 	}
 }
 Triangle::~Triangle()
@@ -79,11 +60,9 @@ Triangle::~Triangle()
 int main()
 {
 	Triangle triangle(3.0, 4.0, 5.0);
-	cout << triangle.Existence() << "\n";
 	cout << triangle.existence << "\n";
-	cout << triangle.Area() << "\n";
 	cout << triangle.area << "\n";
-	cout << triangle.Perimeter() << "\n";
+	cout << triangle.perimeter << "\n";
 	getchar();
 	getchar();
 	return 0;
